5ba499e4c8fee70068924db07900e23e31cb0b21
Blurry <114656831+BlurryGuy@users.noreply.github.com>
2022-10-15 13:55:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+text = """
+   __  __    _         _                            __            __ 
+  / /_/ /_  (_)____   (_)____   ____ ___  __  __   / /____  _  __/ /_
+ / __/ __ \/ / ___/  / / ___/  / __ `__ \/ / / /  / __/ _ \| |/_/ __/
+/ /_/ / / / (__  )  / (__  )  / / / / / / /_/ /  / /_/  __/>  </ /_  
+\__/_/ /_/_/____/  /_/____/  /_/ /_/ /_/\__, /   \__/\___/_/|_|\__/  
+                                       /____/                        
+""" # this variable is named text and its multi-lined so you can put ascii text for example.
+print(text) # printing the string inside of the variable!
+input() # stopping console from closing.
